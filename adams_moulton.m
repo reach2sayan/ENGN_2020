193bc0b16172f2fac0a14360019df481d9463cc9
Sayan Samanta <reach2sayan@gmail.com>
2017-03-13 21:19:18 -0400

    updated Adam's Moulton

--- adams_moulton.m
+++ adams_moulton.m
@@ -1,7 +1,26 @@
-function [ output_args ] = Untitled( input_args )
+function y = adams_moulton(vdp1,x,h,y0)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
+n = length(x);
+y = zeros(n,2);
 
+y(1,:) = y0;
 
+for i = 2:n
+    if i == 2
+        y(i,:) = y(i-1,:) + (h/24)*(55*vdp1(x,y(i-1,:)))';
+        y(i,:) = y(i-1,:) + (h/24)*(9*vdp1(x,y(i,:)) + 19*vdp1(x,y(i-1,:)))';
+    elseif i == 3
+        y(i,:) = y(i-1,:) + (h/24)*(55*vdp1(x,y(i-1,:)) - 59*vdp1(x,y(i-2,:)))';
+        y(i,:) = y(i-1,:) + (h/24)*(9*vdp1(x,y(i,:)) + 19*vdp1(x,y(i-1,:)) - 5*vdp1(x,y(i-2,:)))';
+    elseif i == 4
+        y(i,:) = y(i-1,:) + (h/24)*(55*vdp1(x,y(i-1,:)) - 59*vdp1(x,y(i-2,:)) + 37*vdp1(x,y(i-3,:)))';
+        y(i,:) = y(i-1,:) + (h/24)*(9*vdp1(x,y(i,:)) + 19*vdp1(x,y(i-1,:)) - 5*vdp1(x,y(i-2,:)) + vdp1(x,y(i-3,:)))';
+    else
+        y(i,:) = y(i-1,:) + (h/24)*(55*vdp1(x,y(i-1,:)) - 59*vdp1(x,y(i-2,:)) + 37*vdp1(x,y(i-3,:)) - 9*vdp1(x,y(i-4,:)))';
+        y(i,:) = y(i-1,:) + (h/24)*(9*vdp1(x,y(i,:)) + 19*vdp1(x,y(i-1,:)) - 5*vdp1(x,y(i-2,:)) + vdp1(x,y(i-3,:)))';
+    end
+end
+        
 end
 
--- vdp1.m
+++ vdp1.m
@@ -1,7 +1,7 @@
 function dydx = vdp1(x,y)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-dydx = [-y(1) + y(2); -y(1)-y(2)];
+dydx = [y(2); -2*y(2)-0.75*y(1)];
 
 end
 
